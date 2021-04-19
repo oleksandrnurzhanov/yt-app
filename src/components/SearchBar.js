a401318a65e1b259f9ef6f1bd026632e5ffd0c1b
Oleksandr Nurzhanov <onurzhanov@atlassian.com>
2021-04-19 09:09:17 +0300

    label is updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form className="ui form" onSubmit={onSubmit}>
         <div className="field">
-          <label htmlFor="search">Video Search</label>
+          <label htmlFor="search">Video Search Label</label>
           <input
             id="search"
             type="text"
